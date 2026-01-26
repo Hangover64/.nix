647fa862f2f88b2566f14e18c00779c7ec48437c
Hangover64 <hendrikfarnkopf@pm.me>
2026-01-26 12:44:39 +0100

    commenting own qshell config

--- home.nix
+++ home.nix
@@ -65,7 +65,7 @@
   #hyprland
   xdg.configFile."hypr".source = ./dotfiles/hypr;
   xdg.configFile."wofi".source = ./dotfiles/wofi;
-  xdg.configFile."quickshell".source = ./dotfiles/quickshell;
+  #xdg.configFile."quickshell".source = ./dotfiles/quickshell;
 
   #starship
   programs.starship = {
