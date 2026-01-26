793d6b8767f839b565b2df5c578652259bee9adf
Hangover64 <hendrikfarnkopf@pm.me>
2026-01-26 18:52:20 +0100

    systemd.user.services.wallpaper-rotationn

--- wallpaper-rotator.nix
+++ wallpaper-rotator.nix
@@ -1,26 +1,29 @@
 { config, pkgs, inputs, ...}:
 {
-systemd.user.services.wallpaper-rotation = {
-	Unit = {
-		Description = "Rotate wallpapers automatically";
-		};
-	
-	Service = {
-		Type = "oneshot";
-		ExecStart = "caelestia wallpaper -r";
-		};
-	};
 systemd.user.timers.wallpaper-rotation = {
-	Unit = {
-		Description = "Rotate wallpapers every X minutes";
-		};
-	Timer = {
-		OnBootSec = "5min";
-		OnUnitActiveSec = "5min";
-		Persistent = true;
-		};
-	Install = {
-		WantedBy = [ "timers.target" ];
-		};
-	};
+  Unit = {
+    Description = "Rotate wallpapers every 30 minutes";
+  };
+  
+  Timer = {
+    OnBootSec = "5min";
+    OnUnitActiveSec = "30min";
+    Persistent = true;
+  };
+  
+  Install = {
+    WantedBy = [ "timers.target" ];
+  };
+};
+
+systemd.user.services.wallpaper-rotation = {
+  Unit = {
+    Description = "Rotate wallpapers automatically";
+  };
+  
+  Service = {
+    Type = "oneshot";
+    ExecStart = "caelestia wallpaper -r";
+  };
+};
 }
