b216708c0ff1a2d91a57b0054f67ba4ab3fbb00a
Hangover64 <hendrikfarnkopf@pm.me>
2026-01-26 12:31:46 +0100

    test caelestia

--- flake.nix
+++ flake.nix
@@ -1,12 +1,17 @@
 {
-  description = "Nixos config flake";
+description = "Nixos config flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-    	url = "github:nix-community/home-manager";
-    	inputs.nixpkgs.follows = "nixpkgs";
+inputs = {
+	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	caelestia-shell = {
+		url = "github:caelestia-dots/shell";
+		inputs.nixpkgs.follows = "nixpkgs";
+		};
+	
+	home-manager = {
+    		url = "github:nix-community/home-manager";
+    		inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
