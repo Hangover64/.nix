0cae67dac7e84a6573be40a4cae9403355e03978
Hangover64 <hendrikfarnkopf@pm.me>
2026-01-26 10:34:13 +0100

    quickshell fixes?

--- home.nix
+++ home.nix
@@ -40,7 +40,7 @@
   #hyprland
   xdg.configFile."hypr".source = ./dotfiles/hypr;
   xdg.configFile."wofi".source = ./dotfiles/wofi;
-  xdg.configFile."quickshell".source = ./dotfiles/quickshell;
+  xdg.configFile."quickshell".source = ./dotfiles/quickshell/shell.qml;
 
   #starship
   programs.starship = {
